7464906c3306ddc66204a32cde48938a49cdacc0
RyoFunai <s24s1040du@s.chibakoudai.jp>
2025-09-29 20:58:16 +0900

    点群カットを同時に起動

--- launch/emcl2.launch.py
+++ launch/emcl2.launch.py
@@ -15,6 +15,7 @@ def generate_launch_description():
     #       共有用にする場合は再度 LaunchArgument 化するか、パッケージの share に配置してください。
     hardcoded_map_yaml = '/home/ryo/raspicat_ws/src/maps/mile1_2_50.yaml'
     use_sim_time = LaunchConfiguration('use_sim_time')
+    cutter_params_file = LaunchConfiguration('cutter_params_file')
 
     # map の Launch 引数は不要になったため削除（再利用したい場合は復活させてください）
     declare_use_sim_time = DeclareLaunchArgument(
@@ -28,6 +29,13 @@ def generate_launch_description():
                 get_package_share_directory('emcl2'), 'config', '')),
             TextSubstitution(text='emcl2_with_compressed_map.param.yaml')],
         description='emcl2 param file path')
+    declare_cutter_params = DeclareLaunchArgument(
+        'cutter_params_file',
+        default_value=os.path.join(
+            get_package_share_directory('pointcloud2_cutter'),
+            'config',
+            'tsudanuma.param.yaml'),
+        description='pointcloud2_cutter param file path')
 
     lifecycle_nodes = ['map_server']
 
@@ -45,6 +53,12 @@ def generate_launch_description():
                 executable='static_transform_publisher',
                 name='livox_static_tf',
                 arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'livox_frame'],
+                output='screen'),
+            Node(
+                package='pointcloud2_cutter',
+                executable='pointcloud2_cutter_node',
+                name='pointcloud2_cutter',
+                parameters=[cutter_params_file],
                 output='screen'),
             Node(
                 name='emcl2',
@@ -66,6 +80,7 @@ def generate_launch_description():
     # map 引数宣言は削除済み
     ld.add_action(declare_use_sim_time)
     ld.add_action(declare_params_file)
+    ld.add_action(declare_cutter_params)
 
     ld.add_action(launch_node)
 
