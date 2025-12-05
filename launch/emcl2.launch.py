b6513d959684359d889e6f4589c6ee64b6f2901e
RyoFunai <s24s1040du@s.chibakoudai.jp>
2025-12-05 22:39:39 +0900

    12/5終了時

--- launch/emcl2.launch.py
+++ launch/emcl2.launch.py
@@ -44,19 +44,19 @@ def generate_launch_description():
         default_value=os.path.join(pkg_emcl2, 'rviz2', 'emcl2.rviz'))
 
     declare_map_hdf5 = DeclareLaunchArgument('map_hdf5_file',
-        default_value=os.path.join(pkg_emcl2, 'maps', 'tsukuba20251004_vram8gb.h5'))
+        default_value=os.path.join(pkg_emcl2, 'maps', 'tsukuba_adjusted_float.h5'))
 
     declare_regions_config = DeclareLaunchArgument('regions_config_file',
         default_value=os.path.join(pkg_pointcloud2, 'config', 'tsukuba_regions.yaml'))
 
     declare_waypoint_csv = DeclareLaunchArgument('waypoint_csv_file',
-        default_value=os.path.join(pkg_raspicat_nav, 'maps', 'tsukuba_WP_1.csv'))
+        default_value=os.path.join(pkg_raspicat_nav, 'maps', 'tsukuba_WP.csv'))
 
     declare_map_yaml = DeclareLaunchArgument('map_yaml_file',
-        default_value=os.path.join(pkg_raspicat_nav, 'maps', 'navigation_map_2_edge_stopline.yaml'))
+        default_value=os.path.join(pkg_raspicat_nav, 'maps', 'navigation_map.yaml'))
 
     declare_vq_map = DeclareLaunchArgument('vq_map_file',
-        default_value=os.path.join(pkg_vq_server,'maps', 'tsukuba20251004_vram8gb_voxel05.h5'))
+        default_value=os.path.join(pkg_vq_server,'maps', 'tsukuba_adjusted_float_view.h5'))
 
 
     def launch_setup(context, *args, **kwargs):
