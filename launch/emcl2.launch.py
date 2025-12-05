0305c59020d4e89a064c966cd047ff4005f62cd7
RyoFunai <s24s1040du@s.chibakoudai.jp>
2025-12-06 01:33:19 +0900

    足元のベクトルが0のときの抜け出し機能追加

--- launch/emcl2.launch.py
+++ launch/emcl2.launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
         default_value=os.path.join(pkg_raspicat_nav, 'maps', 'navigation_map.yaml'))
 
     declare_vq_map = DeclareLaunchArgument('vq_map_file',
-        default_value=os.path.join(pkg_vq_server,'maps', 'tsukuba_adjusted_float_view.h5'))
+        default_value=os.path.join(pkg_vq_server,'maps', 'tsukuba_adjusted_fusion_view.h5'))
 
 
     def launch_setup(context, *args, **kwargs):
